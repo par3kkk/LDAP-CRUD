b341adb056b11622867d11bbb872a8d87299a824
par3kkk <1336894784@qq.com>
2020-08-25 15:49:25 +0800

    ldap

--- ldap-back/src/main/java/com/ply/entity/Ldap.java
+++ ldap-back/src/main/java/com/ply/entity/Ldap.java
@@ -1,19 +1,16 @@
 package com.ply.entity;
 
-public class  Ldap {
+public class Ldap {
     // 定义类
-    private int ldapID;
+    public int ldapID;
     private String adName; // 域名
     private String ip; // IP地址
     private String searchPath; // 用户搜索路径
     private String bindUserPath; // 代理用户路径
     private String bindUserPassword; // 代理用户密码
-
     public Ldap ldap;
     // get方法
-    public Ldap(int ldapID){
-        this.ldapID = ldapID;
-    };
+
 
     public int getLdapID() {
         return ldapID;
@@ -21,48 +18,48 @@ public class  Ldap {
 
     public String getadName() {
         return adName;
-    };
+    }
 
     public String getip() {
         return ip;
-    };
+    }
 
     public String getSearchPath() {
         return searchPath;
-    };
+    }
 
     public String getBindUserPath() {
         return bindUserPath;
-    };
+    }
 
     public String getBindUserPassword() {
         return bindUserPassword;
-    };
+    }
 
     // set方法
 
     public void setadName(String adName) {
         this.adName = adName;
-    };
+    }
 
     public void setip(String ip) {
         this.ip = ip;
-    };
+    }
 
     public void setSearchPath(String searchPath) {
         this.searchPath = searchPath;
-    };
+    }
 
     public void setBindUserPath(String bindUserPath) {
         this.bindUserPath = bindUserPath;
-    };
+    }
 
     public void setBindUserPassword(String bindUserPassword) {
         this.bindUserPassword = bindUserPassword;
-    };
+    }
 
-    public int setLdapID(int ldapID) {
-        return ldapID;
+    public void setLdapID(int ldapID) {
+        this.ldapID = ldapID;
     }
 
     @Override
